d0f2086c6c41fddc34cc9668ea337c6dbfb91efa
Paul Newland <paulnewland@pauls-air.lan>
2017-11-15 21:18:12 -0800

    implemented user connection to posts with tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+User.create!(first_name: "Paul", last_name: "Newland", email: "p@example.com", password: "secret", password_confirmation: "secret")
+puts "user created"
+
+10.times do |post|
+  Post.create!(user_id: User.first.id, date: Date.today, rationale: "#{post} rationale content")
 end
-puts "100 posts have been created"
+puts "10 posts have been created"
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "something")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Michael", last_name: "Jordan")
+      @post = Post.create(date: Date.today, rationale: "something", user_id: @user.id)
     end
 
     it "can be created" do
