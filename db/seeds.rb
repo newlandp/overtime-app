c858f647423c569e219734711dd51c9796ecf58f
Paul Newland <paulnewland@pauls-air.lan>
2017-12-06 15:10:34 -0800

    set up administrate dashboard gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 User.create!(first_name: "Paul", last_name: "Newland", email: "p@example.com", password: "secret", password_confirmation: "secret")
-puts "user created"
+User.create!(first_name: "Other", last_name: "User", email: "other@example.com", password: "secret", password_confirmation: "secret")
+puts "2 users created"
 
-10.times do |post|
-  Post.create!(user_id: User.first.id, date: Date.today, rationale: "#{post} rationale content")
+20.times do |post|
+  if post % 2 == 0
+    Post.create!(user_id: User.first.id, date: Date.today, rationale: "#{post} rationale content")
+  else
+    Post.create!(user_id: User.last.id, date: Date.today, rationale: "#{post} rationale content")
+  end
 end
-puts "10 posts have been created"
+puts "20 posts have been created"
