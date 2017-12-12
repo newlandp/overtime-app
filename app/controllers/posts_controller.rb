4e1eef8d4292cbc09e3d9d2e31dd5bb85383103e
Paul Newland <paulnewland@pauls-air.lan>
2017-12-12 10:30:47 -0800

    implement scope for posts by a user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
   
   def index
-    @posts = current_user.posts.includes(:user)
+    @posts = Post.posts_by(current_user)
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
   validates_presence_of :date, :rationale
+
+  def self.posts_by(user)
+    includes(:user).where(user_id: user.id)
+  end
 end
