2b940c69faddae163a154eb2d5509eb139c048ae
Paul Newland <paulnewland@pauls-air.lan>
2017-12-12 20:35:31 -0800

    refactored disabling the edit button to use pundit method

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,12 +1,4 @@
 module PostsHelper
-  def edit_disabled? post
-    if post.submitted? || !current_user.type = "Admin"
-      link_to 'Edit', edit_post_path(post.id), id: "edit_#{post.id}"
-    else
-      link_to 'Edit', edit_post_path(post.id), id: "edit_#{post.id}", class: "disabled"
-    end
-  end
-
   def status_label status
     status_span_generator status
   end
