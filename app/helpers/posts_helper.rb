fb6d16007c247abf2ba1b2b7d660c2e0c927b5d1
Paul Newland <paulnewland@pauls-air.lan>
2017-12-12 20:27:58 -0800

    implemented labels with status values on posts index and disabled editing for approved posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,26 @@
 module PostsHelper
+  def edit_disabled? post
+    if post.submitted?
+      link_to 'Edit', edit_post_path(post.id), id: "edit_#{post.id}"
+    else
+      link_to 'Edit', edit_post_path(post.id), id: "edit_#{post.id}", class: "disabled"
+    end
+  end
+
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+    case status
+    when 'submitted'
+      content_tag(:span, status.titleize, class: "badge badge-primary")
+    when 'approved'
+      content_tag(:span, status.titleize, class: "badge badge-success")
+    when 'rejected'
+      content_tag(:span, status.titleize, class: "badge badge-danger")
+    end
+  end
 end
