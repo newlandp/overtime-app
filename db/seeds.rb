aa8a4e9554195a11708d37d9464e8b791d6e4de2
Paul Newland <paulnewland@pauls-air.lan>
2017-12-17 20:01:31 -0800

    finish rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,30 +7,30 @@ AdminUser.create!(first_name: "Admin",
 
 puts "1 admin user created"
 
-User.create!(first_name: "Paul",
+Employee.create!(first_name: "Paul",
              last_name: "Newland",
              email: "p@example.com",
              password: "secret",
              password_confirmation: "secret",
              phone: '2062717999')
 
-User.create!(first_name: "Other",
+Employee.create!(first_name: "Other",
              last_name: "User",
              email: "other@example.com",
              password: "secret",
              password_confirmation: "secret",
              phone: '2062717999')
 
-puts "2 users created"
+puts "2 employees created"
 
 20.times do |post|
   if post % 2 == 0
-    Post.create!(user_id: User.find_by(first_name: "Paul").id,
+    Post.create!(user_id: Employee.find_by(first_name: "Paul").id,
                  date: Date.today,
                  rationale: "#{post} Rationale. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum maximus massa non dictum. Quisque eu mauris mauris. Maecenas laoreet in dui id cursus. Cras ut imperdiet sem. Integer metus libero, gravida faucibus tortor in, dignissim tristique justo. Donec quis enim non tortor pharetra lobortis. Curabitur iaculis imperdiet massa, eu mollis turpis feugiat a. Praesent vehicula tellus lacus, ac vestibulum augue pellentesque at. Etiam congue ligula nec ipsum vestibulum, eu aliquam nulla porttitor. Duis mollis ultrices dolor, quis semper est consectetur sit amet. Mauris sit amet feugiat massa. Fusce rutrum mauris quam, quis auctor augue volutpat eu. Aenean tincidunt malesuada neque nec vehicula. Proin ac velit in mi bibendum egestas eget in mauris.",
                  overtime_request: 2.5)
   else
-    Post.create!(user_id: User.find_by(first_name: "Other").id,
+    Post.create!(user_id: Employee.find_by(first_name: "Other").id,
                  date: Date.today,
                  rationale: "#{post} Rationale. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fermentum maximus massa non dictum. Quisque eu mauris mauris. Maecenas laoreet in dui id cursus. Cras ut imperdiet sem. Integer metus libero, gravida faucibus tortor in, dignissim tristique justo. Donec quis enim non tortor pharetra lobortis. Curabitur iaculis imperdiet massa, eu mollis turpis feugiat a. Praesent vehicula tellus lacus, ac vestibulum augue pellentesque at. Etiam congue ligula nec ipsum vestibulum, eu aliquam nulla porttitor. Duis mollis ultrices dolor, quis semper est consectetur sit amet. Mauris sit amet feugiat massa. Fusce rutrum mauris quam, quis auctor augue volutpat eu. Aenean tincidunt malesuada neque nec vehicula. Proin ac velit in mi bibendum egestas eget in mauris.",
                  overtime_request: 2.5)
@@ -41,11 +41,11 @@ puts "20 posts have been created"
 
 2.times do |audit_log|
   if audit_log % 2 == 0
-    AuditLog.create!(user_id: User.find_by(first_name: "Paul").id,
+    AuditLog.create!(user_id: Employee.find_by(first_name: "Paul").id,
                      status: 0,
                      start_date: (Date.today - 6.days))
   else
-    AuditLog.create!(user_id: User.find_by(first_name: "Other").id,
+    AuditLog.create!(user_id: Employee.find_by(first_name: "Other").id,
                      status: 0,
                      start_date: (Date.today - 6.days))
   end
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,13 +1,14 @@
 namespace :notification do
   desc "Sends SMS notification to employees asking them to log if they had overtime or not"
   task sms: :environment do
-    # schedule to run every sunday at 5pm
-    # iterate over all employees
-    # skip admin users
-    # send a message that has instructions and a link to log time
-    # User.all.each do |user|
-    #   SmsTool.send_sms(user.phone_number, 'message')
-    # end
+    if Time.now.sunday?
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard to request overtime or confirm your hours for last week: http://newlandp-overtime.herokuapp.com/"
+      
+      employees.all.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
+    end
   end
 
   desc "Sends email notification to managers (admin users) each day to inform of pending overtime requests"
